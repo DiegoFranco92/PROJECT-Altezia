088cb5cac86c182c9df461cc24efccfc995116b4
DiegoFranco92 <diegofranco.daft@gmail.com>
2023-01-31 12:22:26 -0600

    update 31/01/23

---
+++ js.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.querySelector('.cta-button');
+
+// Add click event to button
+button.addEventListener('click', () => {
+  // Show a message when the button is clicked
+  alert('Thank you for your interest in our real estate development. A representative will contact you shortly.');
+});
